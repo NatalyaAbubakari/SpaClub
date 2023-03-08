c551121a5a159186a33d74cc3cb02bf606b4987e
RubyDeveloper1 <ruby_developer1@yahoo.com>
2023-03-08 18:50:02 +0300

    Created 'index' and 'visit' pages

--- app.rb
+++ app.rb
@@ -4,5 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Welcome to our spa club. Our center employs only certified and experienced specialists, masters of their craft. We are waiting for you at a convenient time for you"			
+    erb :index
+end
+
+get '/visit' do
+    erb :visit
 end
