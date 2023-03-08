6baf0ac2a73ca43d74f4f8c81c3eec059a5c6dc6
RubyDeveloper1 <ruby_developer1@yahoo.com>
2023-03-08 13:20:40 +0300

    Changed the greeting on the main page

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Welcome to our spa club. Our center employs only certified and experienced specialists, masters of their craft. We are waiting for you at a convenient time for you"			
 end
